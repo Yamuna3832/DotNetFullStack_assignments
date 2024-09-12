893be54569717ac4d283c3702e12845cd3807306
Yamuna Ampalam <94949218+Yamuna3832@users.noreply.github.com>
2024-09-12 15:54:19 +0530

    Add files via upload

---
+++ WebApplication7/Repository/IProductRepository.cs
@@ -0,0 +1,23 @@
+﻿using WebApplication7.Models;
+
+namespace WebApplication7.Repository
+{
+    public interface IProductRepository
+    {
+        List<Product> GetAllProducts();
+        void CreateProduct(Product obj);
+        Product GetProductById(int id);
+
+     
+
+        
+
+        void EditProduct(Product obj);
+        string DeleteProductById(int id);
+        List<Product> GetOutOfStockProducts();
+        List<Product> GetProductsBetweenPriceRange(int lowerPriceLimit, int upperPriceLimit);
+
+        List<string> GetProductsCategoryNames();
+        List<Product> GetProductsByCategory(string category);
+    }
+}
